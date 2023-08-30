23a6ed49099e0233c3e911898aa8b7a59dda2b30
Raphael <48337836+PhaellZX@users.noreply.github.com>
2023-08-29 21:56:32 -0300

    Add files via upload

---
+++ Aula04/src/pacote/Estudante.java
@@ -0,0 +1,13 @@
+package pacote;
+
+public class Estudante {
+	String nome;
+	String matricula;
+	String curso;
+	
+	public Estudante(String nome, String matricula, String curso){
+		this.nome = nome;
+		this.matricula =  matricula;
+		this.curso = curso;
+	}
+}
