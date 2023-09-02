9ffc901efcfc86158d772b952b8345238c9da584
Raphael <48337836+PhaellZX@users.noreply.github.com>
2023-09-02 10:44:20 -0300

    Add files via upload

---
+++ Aula02_Generics/src/Package/Teste3.java
@@ -0,0 +1,24 @@
+package Package;
+
+public class Teste3 {
+	
+	public static < T > void imprimeVetor(Object v[]) {
+		for(Object e : v) System.out.printf(e + " ");
+			System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Double[] arrayDouble = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6};
+		System.out.println("Vetor de double: ");
+		imprimeVetor(arrayDouble);
+		
+		Integer[] arrayInt = {1, 2, 3, 4, 5, 6};
+		System.out.println("Vetor de int: ");
+		imprimeVetor(arrayInt);
+		
+		Character[] arrayChar = {'E','C','O','0','3','0'};
+		System.out.println("Vetor de char: ");
+		imprimeVetor(arrayChar);
+
+	}
+}
